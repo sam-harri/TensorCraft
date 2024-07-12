b1f29f27ef61a9b0e191c3d7db7dabb78ef7ff61
Sam Harrison <sharr125@uottawa.ca>
2024-07-11 21:55:06 -0400

    refactored sidebar

--- src/components/ToolBoxContent.tsx
+++ src/components/ToolBoxContent.tsx
@@ -97,11 +97,7 @@ const categories = [
   },
 ];
 
-export type ToolBoxContentType = {
-  isCollapsed: boolean;
-};
-
-const ToolBoxContent: React.FC<ToolBoxContentType> = ({isCollapsed}) => {
+const ToolBoxContent: React.FC = () => {
   const [activeDropdown, setActiveDropdown] = useState<string | null>(null);
   const [searchTerm, setSearchTerm] = useState("");
 
@@ -120,97 +116,91 @@ const ToolBoxContent: React.FC<ToolBoxContentType> = ({isCollapsed}) => {
   );
 
   return (
-    <div
-    className={`overflow-auto hide-scrollbar absolute top-0 left-0 h-full transition-width bg-white/30 backdrop-blur-md shadow-md ${isCollapsed ? 'w-0' : 'w-72'} z-20 border-r border-t border-gray-300 rounded-r-3xl`}
-  >
-    {!isCollapsed && (
-      <>
-        {/* search bar */}
-        <div className="flex items-center px-4 my-6">
-          <input
-            type="search"
-            className="flex-grow w-32 relative m-0 block rounded border border-solid border-neutral-200 bg-transparent bg-clip-padding px-3 py-1 text-base font-normal text-surface outline-none transition duration-200 ease-in-out placeholder:text-neutral-500 focus:z-[3] focus:border-primary focus:shadow-inset focus:outline-none motion-reduce:transition-none dark:border-white/10 dark:text-white dark:placeholder:text-neutral-200 dark:autofill:shadow-autofill dark:focus:border-primary"
-            placeholder="Search Toolbox"
-            aria-label="Search"
-            id="exampleFormControlInput2"
-            aria-describedby="button-addon2"
-            value={searchTerm}
-            onChange={handleSearch}
-          />
-          <span
-            className="flex items-center whitespace-nowrap px-3 py-[0.25rem] text-surface dark:border-neutral-400 dark:text-white [&>svg]:h-5 [&>svg]:w-5"
-            id="button-addon2"
+    <>
+      {/* search bar */}
+      <div className="flex items-center px-4 my-6">
+        <input
+          type="search"
+          className="flex-grow w-32 relative m-0 block rounded border border-solid border-neutral-200 bg-transparent bg-clip-padding px-3 py-1 text-base font-normal text-surface outline-none transition duration-200 ease-in-out placeholder:text-neutral-500 focus:z-[3] focus:border-primary focus:shadow-inset focus:outline-none motion-reduce:transition-none dark:border-white/10 dark:text-white dark:placeholder:text-neutral-200 dark:autofill:shadow-autofill dark:focus:border-primary"
+          placeholder="Search Toolbox"
+          aria-label="Search"
+          id="exampleFormControlInput2"
+          aria-describedby="button-addon2"
+          value={searchTerm}
+          onChange={handleSearch}
+        />
+        <span
+          className="flex items-center whitespace-nowrap px-3 py-[0.25rem] text-surface dark:border-neutral-400 dark:text-white [&>svg]:h-5 [&>svg]:w-5"
+          id="button-addon2"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth="2"
+            stroke="currentColor"
           >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth="2"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z"
-              />
-            </svg>
-          </span>
-        </div>
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z"
+            />
+          </svg>
+        </span>
+      </div>
 
-        <hr className="border-gray-200" />
-        <div className="flex flex-col px-4">
-          {searchTerm === "" ? (
-            categories.map(category => (
-              <>
-                <div key={category.key} className="my-3">
-                  <div
-                    className="flex items-center justify-between p-2 cursor-pointer rounded"
-                    onClick={() => toggleDropdown(category.key)}
-                  >
-                    <span>{category.title}</span>
-                    <Dropdown isCollapsed={activeDropdown !== category.key} />
+      <hr className="border-gray-200" />
+      <div className="flex flex-col px-4">
+        {searchTerm === "" ? (
+          categories.map((category) => (
+            <>
+              <div key={category.key} className="my-3">
+                <div
+                  className="flex items-center justify-between p-2 cursor-pointer rounded"
+                  onClick={() => toggleDropdown(category.key)}
+                >
+                  <span>{category.title}</span>
+                  <Dropdown isCollapsed={activeDropdown !== category.key} />
+                </div>
+                {activeDropdown === category.key && (
+                  <div className="px-2 space-y-3">
+                    {category.items.map((item) => (
+                      <>
+                        <InputElement
+                          key={item.nodeClass}
+                          label={item.label}
+                          img={item.img}
+                          nodeClass={item.nodeClass}
+                        />
+                      </>
+                    ))}
                   </div>
-                  {activeDropdown === category.key && (
-                    <div className="px-2 space-y-3">
-                      {category.items.map(item => (
-                        <>
-                          <InputElement
-                            key={item.nodeClass}
-                            label={item.label}
-                            img={item.img}
-                            nodeClass={item.nodeClass}
-                          />
-                        </>
-                      ))}
-                    </div>
-                  )}
-                </div>
-                <div className='flex justify-center'>
-                  <hr className=" border-gray-200 w-3/4 mr-4" />
-                </div>
-                </>
-            ))
-          ) : (
-            <div className="px-2 space-y-3 mt-3">
-              {filteredCategories.map(item => (
-                <>
+                )}
+              </div>
+              <div className="flex justify-center">
+                <hr className=" border-gray-200 w-3/4 mr-4" />
+              </div>
+            </>
+          ))
+        ) : (
+          <div className="px-2 space-y-3 mt-3">
+            {filteredCategories.map((item) => (
+              <>
                 <InputElement
                   key={item.nodeClass}
                   label={item.label}
                   img={item.img}
                   nodeClass={item.nodeClass}
                 />
-                                    <div className='flex justify-center'>
+                <div className="flex justify-center">
                   <hr className=" border-gray-200 w-3/4 mx-2" />
                 </div>
-                </>
-              ))}
-            </div>
-          )}
-        </div>
-      </>
-    )}
-  </div>
+              </>
+            ))}
+          </div>
+        )}
+      </div>
+    </>
   );
 };
 

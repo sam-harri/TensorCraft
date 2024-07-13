f15dcdd8f788291aac2694fde32916633917154e
Sam Harrison <sharr125@uottawa.ca>
2024-07-13 18:09:13 -0400

    refactored tanh

--- src/nodes/ActivationFunctions/SigmoidNode.tsx
+++ src/nodes/ActivationFunctions/SigmoidNode.tsx
@@ -1,10 +1,8 @@
-// src/components/SigmoidLayerNode.jsx
 import React, { useState, useEffect } from 'react';
 import { NodeProps, Handle, Position } from 'reactflow';
-import Dropdown from '../../components/Dropdown';
 import useGraphStore from '../../state/graphStore';
-import Hint from '../../components/Hint';
-import DeleteNode from '../../components/DeleteNode';
+import NodeHeader from '../../components/NodeHeader';
+import ShapeLabel from '../../components/ShapeLabel';
 
 export type SigmoidLayerNodeData = {
   inputShape: string | null;
@@ -22,46 +20,16 @@ const SigmoidNode: React.FC<NodeProps<SigmoidLayerNodeData>> = (props) => {
     } else {
       updateNodeData(props.id, { outputShape: 'Not Connected' });
     }
-  }, [props.data.inputShape, updateNodeData, props.id]);
+  }, [props.data.inputShape, updateNodeData]);
 
   return (
     <div className="bg-white shadow-md rounded border border-gray-300 w-72 relative">
       <Handle type="target" position={Position.Left} isConnectable={true} />
-      <div
-        className="flex items-center p-1 cursor-pointer"
-        onClick={() => setIsCollapsed(!isCollapsed)}
-      >
-        <img
-          src="activationfunctions/sigmoid.png"
-          alt="Sigmoid Activation"
-          className="rounded-full h-10 w-10 mr-4"
-        />
-        <div className="flex-grow font-semibold text-gray-800 text-left">Sigmoid</div>
-        <DeleteNode nodeId={props.id} />
-        <div className="text-gray-500 ml-2">
-          <Dropdown isCollapsed={isCollapsed} />
-        </div>
-      </div>
+      <NodeHeader title="Sigmoid" isCollapsed={isCollapsed} setIsCollapsed={setIsCollapsed} imageSource="activationfunctions/sigmoid.png" imageAlt="Sigmoid Activation" props={props} />
       {!isCollapsed && (
         <div className="p-4 border-t border-gray-200">
-          <div className="mb-4">
-            <label className="block text-gray-600 text-sm">Input Shape:</label>
-            <div className="flex items-center">
-              <div className="mt-1 block w-full rounded border-gray-300 shadow-sm sm:text-sm bg-gray-100 p-2">
-                {props.data.inputShape ? props.data.inputShape : 'Not Connected'}
-              </div>
-              <Hint message="The shape of the input tensor." />
-            </div>
-          </div>
-          <div className="mb-4">
-            <label className="block text-gray-600 text-sm">Output Shape:</label>
-            <div className="flex items-center">
-              <div className="mt-1 block w-full rounded border-gray-300 shadow-sm sm:text-sm bg-gray-100 p-2">
-                {props.data.outputShape ? props.data.outputShape : 'Not Connected'}
-              </div>
-              <Hint message="The shape of the output tensor. Same as input shape." />
-            </div>
-          </div>
+          <ShapeLabel input={true} shape={props.data.inputShape || 'Not Connected'} shapeHintMessage="The shape of the input tensor." />
+          <ShapeLabel input={false} shape={props.data.outputShape || 'Not Connected'} shapeHintMessage="The shape of the output tensor. Same as input shape." />
         </div>
       )}
       <Handle type="source" position={Position.Right} isConnectable={true} />

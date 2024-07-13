62a9c6cc3a2aa0a438a4f87a1c8404392032f5aa
Sam Harrison <sharr125@uottawa.ca>
2024-07-13 18:05:37 -0400

    relu refactor

--- src/nodes/ActivationFunctions/ReluNode.tsx
+++ src/nodes/ActivationFunctions/ReluNode.tsx
@@ -1,10 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import { NodeProps, Handle, Position } from 'reactflow';
-import Dropdown from '../../components/Dropdown';
 import useGraphStore from '../../state/graphStore';
 import Hint from '../../components/Hint';
 import Checkbox from '../../components/Checkbox';
-import DeleteNode from '../../components/DeleteNode';
+import NodeHeader from '../../components/NodeHeader';
+import ShapeLabel from '../../components/ShapeLabel';
 
 export type ReLULayerNodeData = {
   inputShape: string | null;
@@ -14,21 +14,18 @@ export type ReLULayerNodeData = {
 
 const ReLULayerNode: React.FC<NodeProps<ReLULayerNodeData>> = (props) => {
   const updateNodeData = useGraphStore((state) => state.updateNodeData);
-  const nodes = useGraphStore((state) => state.nodes);
-  console.table(nodes);
   const [isCollapsed, setIsCollapsed] = useState(true);
 
   const { inplace } = props.data;
 
   useEffect(() => {
-    console.log(`ReluNode props.data ${props.data.inputShape}`)
     const inputShape = props.data.inputShape;
     if (inputShape) {
       updateNodeData(props.id, { outputShape: inputShape });
     } else {
       updateNodeData(props.id, { outputShape: 'Not Connected' });
     }
-  }, [props.data.inputShape, updateNodeData, props.id]);
+  }, [props.data.inputShape, updateNodeData]);
 
   const handleInplaceChange = (checked: boolean) => {
     updateNodeData(props.id, { inplace: checked });
@@ -37,46 +34,16 @@ const ReLULayerNode: React.FC<NodeProps<ReLULayerNodeData>> = (props) => {
   return (
     <div className="bg-white shadow-md rounded border border-gray-300 w-72 relative">
       <Handle type="target" position={Position.Left} isConnectable={true} />
-      <div
-        className="flex items-center p-1 cursor-pointer"
-        onClick={() => setIsCollapsed(!isCollapsed)}
-      >
-        <img
-          src="activationfunctions/relu.png"
-          alt="ReLU Activation"
-          className="rounded-full h-10 w-10 mr-4"
-        />
-        <div className="flex-grow font-semibold text-gray-800 text-left">ReLU</div>
-        <DeleteNode nodeId={props.id} />
-        <div className="text-gray-500 ml-2">
-          <Dropdown isCollapsed={isCollapsed} />
-        </div>
-      </div>
+      <NodeHeader title="ReLU" isCollapsed={isCollapsed} setIsCollapsed={setIsCollapsed} imageSource="activationfunctions/relu.png" imageAlt="ReLU Activation" props={props} />
       {!isCollapsed && (
         <div className="p-4 border-t border-gray-200">
-          <div className="mb-4">
-            <label className="block text-gray-600 text-sm">Input Shape:</label>
-            <div className="flex items-center">
-              <div className="mt-1 block w-full rounded border-gray-300 shadow-sm sm:text-sm bg-gray-100 p-2">
-                {props.data.inputShape ? props.data.inputShape : 'Not Connected'}
-              </div>
-              <Hint message="The shape of the input tensor." />
-            </div>
-          </div>
-          <div className="mb-4">
-            <label className="block text-gray-600 text-sm">Output Shape:</label>
-            <div className="flex items-center">
-              <div className="mt-1 block w-full rounded border-gray-300 shadow-sm sm:text-sm bg-gray-100 p-2">
-                {props.data.outputShape ? props.data.outputShape : 'Not Connected'}
-              </div>
-              <Hint message="The shape of the output tensor. Same as input shape." />
-            </div>
-          </div>
           <div className="mb-2 flex items-center">
             <label className="block text-gray-600 text-sm mr-2">Inplace:</label>
             <Checkbox id="inplaceCheck" checked={inplace} onChangeFunction={handleInplaceChange} />
             <Hint message="Modifies the input data directly to save memory. Only use when the input data is no longer needed after." />
           </div>
+          <ShapeLabel input={true} shape={props.data.inputShape || 'Not Connected'} shapeHintMessage="The shape of the input tensor." />
+          <ShapeLabel input={false} shape={props.data.outputShape || 'Not Connected'} shapeHintMessage="The shape of the output tensor. Same as input shape." />
         </div>
       )}
       <Handle type="source" position={Position.Right} isConnectable={true} />
